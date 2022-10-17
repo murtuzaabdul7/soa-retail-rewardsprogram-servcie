d000f4ed7edccbe463277cbab3dc85924d2bcbd0
MBhaiji1 <murtuza.bhaiji1@t-mobile.com>
2022-10-17 16:48:18 -0400

    Added Postman collection Added parameterized test to RewardPointsServiceImpl

--- src/test/java/com/assessment/rewardprogram/service/RewardPointsServiceImplTest.java
+++ src/test/java/com/assessment/rewardprogram/service/RewardPointsServiceImplTest.java
@@ -1,21 +1,41 @@
 package com.assessment.rewardprogram.service;
 
 
+import com.assessment.rewardprogram.domain.request.PeriodRange;
+import com.assessment.rewardprogram.domain.request.RewardPointsRequest;
 import com.assessment.rewardprogram.domain.response.RewardPointsDetailsResponse;
+import com.assessment.rewardprogram.entity.SalesOrderDetails;
+import com.assessment.rewardprogram.repository.SalesOrderDetailsRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.Mockito.when;
 
 @Slf4j
+@ExtendWith(MockitoExtension.class)
 public class RewardPointsServiceImplTest {
 
     private RewardPointsServiceImpl rewardPointsService;
 
+    @Mock
+    private SalesOrderDetailsRepository salesOrderDetailsRepository;
+
+
     @BeforeEach
     public void beforeEach() {
-        rewardPointsService = new RewardPointsServiceImpl();
+        rewardPointsService = new RewardPointsServiceImpl(salesOrderDetailsRepository);
     }
 
     @ParameterizedTest
@@ -27,12 +47,37 @@ public class RewardPointsServiceImplTest {
     }
 
 
-//    @ParameterizedTest
-//    @CsvFileSource(resources = "/calculateRewardPointsData.csv", numLinesToSkip = 1)
-//    public void calculateRewardPointsTest(double totalpurchaseamount, double expected) {
-//        RewardPointsDetailsResponse actual = rewardPointsService.getRewardPoints(null);
-//        log.info("totalpurchaseamount: {}, expectedValue: {}, actualValue: {}", totalpurchaseamount, expected, actual);
-////        Assertions.assertEquals(expected, actual, 0.1);
-//    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "/calculateRewardPointsData.csv", numLinesToSkip = 1)
+    public void calculateRewardPointsTest(String salesordernumber, String customerid, double totalpurchaseamount,
+                                          boolean rewardpointseligible, String startYear, String startMonth,
+                                          String endYear, String endMonth, double expectedValue) {
+        RewardPointsRequest request = new RewardPointsRequest();
+        request.setCustomerIdList(Collections.singletonList(customerid));
+        request.setStartRange(new PeriodRange(startYear, startMonth));
+
+        if (endYear.equals("null") || endMonth.equals("null")) {
+            request.setEndRange(null);
+        } else {
+            request.setStartRange(new PeriodRange(endYear, endMonth));
+        }
+
+        SalesOrderDetails salesOrderDetails = new SalesOrderDetails();
+        salesOrderDetails.setSalesOrderNumber(salesordernumber);
+        salesOrderDetails.setCustomerId(customerid);
+        salesOrderDetails.setTotalPurchaseAmount(totalpurchaseamount);
+        salesOrderDetails.setRewardPointsEligible(rewardpointseligible);
+
+        List<SalesOrderDetails> salesOrderDetailsList = new ArrayList<>();
+        salesOrderDetailsList.add(salesOrderDetails);
+
+        when(salesOrderDetailsRepository.findAllByCustomerIdByRange(
+                any(), any(), any(), anyBoolean()
+        )).thenReturn(salesOrderDetailsList);
+
+        RewardPointsDetailsResponse actual = rewardPointsService.getRewardPoints(request);
+        log.info("totalpurchaseamount: {}, expectedValue: {}, actualValue: {}", totalpurchaseamount,
+                expectedValue, actual.getRewardPointsDetailsList().get(0).getTotalRewardPoints());
+    }
 
 }
